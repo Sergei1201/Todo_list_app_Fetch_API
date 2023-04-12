113dcf6365e2fd6cd045a45b94726d329b711e40
Sergei Sokolov <sokolov12ssa@gmail.com>
2023-04-12 22:15:31 +0500

    update & delete todos from JSON API

--- app.js
+++ app.js
@@ -1,37 +1,44 @@
 const apiUrl = 'https://jsonplaceholder.typicode.com/todos'
 
-// Get Todos from JSON placeholder
+// Get Todos
 const getTodos = () => {
     fetch(apiUrl + '?_limit=5')
     .then(res => res.json())
     .then(data => {
-        // Display todos in the UI
-        data.forEach((todo) => addTodoToDom(todo))
+        data.forEach(todo => {
+            addTodosToDOM(todo)
+        })
     })
 }
 
-// Add Todo to DOM
-const addTodoToDom = (todo) => {
-    // Create a new element
+// Show Todos
+const addTodosToDOM = (todo) => {
+    // Create a new DOM element
     const div = document.createElement('div')
     // Add a text node
     div.appendChild(document.createTextNode(todo.title))
     // Add a specific class
+    div.classList.add('todo')
+    // Set attribute
+    div.setAttribute('data-id', todo.id)
+    // Check for todo completed
     if (todo.completed === true) {
         div.classList.add('done')
-    } 
-    // Append to DOM
+    }
+    // Append to the DOM
     document.getElementById('todo-list').appendChild(div)
+
 }
 
 // Create Todo
 const createTodo = (e) => {
     e.preventDefault()
+    // Create a variable that represents a new todo
     const newTodo = {
         title: e.target.firstElementChild.value,
         completed: false
     }
-    // Make POST request to JSON placeholder to post the todo
+    // Make a POST request to JSON API
     fetch(apiUrl, {
         method: 'POST',
         body: JSON.stringify(newTodo),
@@ -40,15 +47,50 @@ const createTodo = (e) => {
         }
     })
     .then(res => res.json())
-    .then(data => {
-        addTodoToDom(data)
-        e.target.firstElementChild.value = ''
+    .then(data => addTodosToDOM(data))
+    e.target.firstElementChild.value = ''
+}
+
+// Update Todo
+const updateTodo = (e) => {
+    if (e.target.classList.contains('todo')) {
+        e.target.classList.toggle('done')
+         updateCompleted(e.target.dataset.id, e.target.classList.contains('todo'))
+    }
+   
+}
+
+// Update Completed
+const updateCompleted = (id, completed) => {
+    fetch(`${apiUrl}/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            completed
+        })
     })
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+
+// Remove Todo
+const removeTodo = (e) => {
+    if (e.target.classList.contains('todo')) {
+        const id = e.target.dataset.id
+        fetch(`${apiUrl}/${id}`, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(() => e.target.remove())
+    }
+    
+
 }
 
 const init = () => {
     document.addEventListener('DOMContentLoaded', getTodos)
-    document.getElementById('todo-form').addEventListener('submit', createTodo)
+    document.querySelector('#todo-form').addEventListener('submit', createTodo)
+    document.querySelector('#todo-list').addEventListener('click', updateTodo)
+    document.querySelector('#todo-list').addEventListener('dblclick', removeTodo)
 }
 
 init()
